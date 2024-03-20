67b2afd1666c9a9a979bc41112a79452bd9437f4
Terry9022 <karta1020376@gmail.com>
2024-03-19 23:38:06 -0400

    booklist exercise intro

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-  return <h1>Hello World</h1>;
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
+const Book = () => {
+  const title = "Dog Man: The Scarlet Shedder";
+  const author = "Dav Pilkey";
+  return (
+    <article className="book">
+      <img src="./images/book-1.jpg" alt="Dog Man: The Scarlet Shedder" />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+
+root.render(<BookList />);
