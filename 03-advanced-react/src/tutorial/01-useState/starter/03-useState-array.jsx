6c3649f454bb4da85a80cefd1d8d4aade4d53222
Terry9022 <karta1020376@gmail.com>
2024-04-30 13:52:24 +0800

    finish useState Hook part

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -4,8 +4,13 @@ import React from "react";
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
 
-  const removeItem = () => {};
-  const clearAllItems = () => {};
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearAllItems = () => {
+    setPeople([]);
+  };
   return (
     <div>
       {people.map((person) => {
@@ -13,9 +18,20 @@ const UseStateArray = () => {
         return (
           <div key={id} className="item">
             <h4>{name}</h4>
+            <button type="button" onClick={() => removeItem(id)}>
+              remove
+            </button>
           </div>
         );
       })}
+      <button
+        type="button"
+        className="btn"
+        style={{ marginTop: "2rem" }}
+        onClick={clearAllItems}
+      >
+        clear all
+      </button>
     </div>
   );
 };
