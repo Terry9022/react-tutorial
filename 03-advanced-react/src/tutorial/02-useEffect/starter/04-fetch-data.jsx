3ccc1a97eb75f4234c22970bd232a8ba0efea0c1
Terry9022 <karta1020376@gmail.com>
2024-04-30 14:29:57 +0800

    start useEffect basic

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,11 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {}, []);
   return <h2>fetch data example</h2>;
 };
 export default FetchData;
