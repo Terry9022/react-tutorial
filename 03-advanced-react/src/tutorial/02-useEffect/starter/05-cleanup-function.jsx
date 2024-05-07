6ffa09e7d9e7b2fb2d9daeba02aa410837e88339
Terry9022 <karta1020376@gmail.com>
2024-05-07 20:29:58 +0800

    finish useEffect Hook part

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        toggle component
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    const someFunc = () => {
+      console.log("hmm, this is interesting");
+    };
+    window.addEventListener("scroll", someFunc);
+    return () => window.removeEventListener("scroll", someFunc);
+  }, []);
+  return <h1>hello there</h1>;
 };
 
 export default CleanupFunction;
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,17 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
+  // while fetching data
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      // done fetching data
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  
   return <h2>Multiple Returns Basics</h2>;
 };
 export default MultipleReturnsBasics;
